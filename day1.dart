8af2799affd35fcce6939d09a3802d620198dc7d
Oğuzhan Erbaş <139886655+erbasdev@users.noreply.github.com>
2024-02-06 22:28:19 +0300

    Add files via upload

---
+++ day1.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  final String mesaj = "Hello World!";
+  print(mesaj);
+  final pi = 3.14;
+  print(pi);
+  const int sene = 2022;
+  print(sene);
+  final int user1Money = 100;
+  int user2Money = 100;
+  print(user1Money + user2Money);
+}
+
+/*
+Sabitler (Constant)
+- Sabitler, değiştirilemeyen değerlerdir.
+- final ve const anahtar kelimeleri ile tanımlanır.
+- final ile uygulama çalışırken değer atanır.
+- const ile derleme anında değer atanır.
+- const sınıf içinde kullanılmaz.
+- Genelde final kullanılır.
+- 
+*/
